dff91eb5f8f001c344866244c109c8a027a1e35c
Jelev <julienjelev2@gmail.com>
2022-09-28 12:31:26 +0300

    changeCreate.cshtml

--- VetSystem.Core/ViewModels/Animal/AddAnimalViewModel.cs
+++ VetSystem.Core/ViewModels/Animal/AddAnimalViewModel.cs
@@ -7,19 +7,30 @@
     {     
         public string Name { get; set; }
 
+        [Display(Name ="Category")]
         public string CategoryName { get; set; }
 
         public int Age { get; set; }
 
         public int Kilograms { get; set; }
-   
+
+        [Display(Name = "Breed")]
+
         public string? BreedName { get; set; }
+
+        [Display(Name = "Disease")]
 
         public string? DiseaseName { get; set; }
 
+        [Display(Name = "Owner - First Name")]
+
         public string? OwnerFirstName { get; set; }
 
+        [Display(Name = "Owner - Last Name")]
+
         public string? OwnerLastName { get; set; }
+
+        [Display(Name = "Medication")]
 
         public string? MedicationName { get; set; }
     }
