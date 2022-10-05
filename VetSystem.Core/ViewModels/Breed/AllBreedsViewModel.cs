9af067569f0487e468ebd88248a77d02d38d5b4d
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2022-10-05 19:58:24 +0300

    ..

--- VetSystem.Core/ViewModels/Breed/AllBreedsViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VetSystem.Core.ViewModels.Breed
-{
-    public class AllBreedsViewModel
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-    }
-}
--- VetSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace VetSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
